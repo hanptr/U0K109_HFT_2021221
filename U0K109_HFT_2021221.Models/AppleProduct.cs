8e6b37e1969de872e4f6f9b2b0ef8dfcab70b4b9
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-10-17 11:28:10 +0200

    AppleProduct class done

--- U0K109_HFT_2021221.Models/AppleProduct.cs
+++ U0K109_HFT_2021221.Models/AppleProduct.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace U0K109_HFT_2021221.Models
 {
+    public enum Type {Mac, iPhone, iPad, AppleWatch}
     public class AppleProduct
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Serial { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Date { get; set; }
+        public Type Type { get; set; }
+        public string Color { get; set; }
+
     }
 }
