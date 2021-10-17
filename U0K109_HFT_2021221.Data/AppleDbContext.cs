9d4f5951ffd48e505d0494e8bfc6b76add05c850
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-10-17 16:34:41 +0200

    AppleDbContext products modified

--- U0K109_HFT_2021221.Data/AppleDbContext.cs
+++ U0K109_HFT_2021221.Data/AppleDbContext.cs
@@ -55,19 +55,19 @@ namespace U0K109_HFT_2021221.Data
                     .OnDelete(DeleteBehavior.Restrict);
             });
 
-            AppleService a1 = new AppleService() { ServiceID = 1, ServiceName = "A1", Location = "Budapest" };
+            AppleService a1 = new AppleService() { ServiceID = 1, ServiceName = "A1", Location = "Budapest"};
             AppleService a2 = new AppleService() { ServiceID = 2, ServiceName = "A2", Location = "Érd" };
 
-            Customer Bela = new Customer() { CustomerID = 1, Name = "Bela Bela", Email = "bela@gmail.com" };
-            Customer Sanyi = new Customer() { CustomerID = 3, Name = "Sanyi Sanyi", Email = "Sanyi@gmail.com" };
-            Customer Lajos = new Customer() { CustomerID = 5, Name = "Lajos Lajos", Email = "Lajos@gmail.com" };
+            Customer Bela = new Customer() { CustomerID = 1, Name = "Bela Bela", Email = "bela@gmail.com", ServiceID=1  };
+            Customer Sanyi = new Customer() { CustomerID = 3, Name = "Sanyi Sanyi", Email = "Sanyi@gmail.com", ServiceID=1 };
+            Customer Lajos = new Customer() { CustomerID = 5, Name = "Lajos Lajos", Email = "Lajos@gmail.com", ServiceID=2 };
 
-            AppleProduct mac1 = new AppleProduct() { Serial = 1111, Color = "Black", Type = Models.Type.Mac };
-            AppleProduct iphone1 = new AppleProduct() { Serial = 1112, Color = "Red", Type = Models.Type.iPhone };
-            AppleProduct watch1 = new AppleProduct() { Serial = 1113, Color = "Green", Type = Models.Type.AppleWatch };
-            AppleProduct mac2 = new AppleProduct() { Serial = 1114, Color = "Black", Type = Models.Type.Mac };
-            AppleProduct ipad1 = new AppleProduct() { Serial = 1115, Color = "Yellow", Type = Models.Type.iPad };
-            AppleProduct iphone2 = new AppleProduct() { Serial = 1116, Color = "Blue", Type = Models.Type.iPhone };
+            AppleProduct mac1 = new AppleProduct() { Serial = 1111, Color = "Black", Type = Models.Type.Mac, ServiceID=1, CustomerID=1 };
+            AppleProduct iphone1 = new AppleProduct() { Serial = 1112, Color = "Red", Type = Models.Type.iPhone, ServiceID=1, CustomerID = 1 };
+            AppleProduct watch1 = new AppleProduct() { Serial = 1113, Color = "Green", Type = Models.Type.AppleWatch, ServiceID = 1, CustomerID = 3 };
+            AppleProduct mac2 = new AppleProduct() { Serial = 1114, Color = "Black", Type = Models.Type.Mac, ServiceID = 1, CustomerID = 3 };
+            AppleProduct ipad1 = new AppleProduct() { Serial = 1115, Color = "Yellow", Type = Models.Type.iPad, ServiceID = 2, CustomerID = 5 };
+            AppleProduct iphone2 = new AppleProduct() { Serial = 1116, Color = "Blue", Type = Models.Type.iPhone, ServiceID = 2, CustomerID = 5 };
 
             modelBuilder.Entity<AppleService>().HasData(a1, a2);
             modelBuilder.Entity<Customer>().HasData(Bela, Sanyi, Lajos);
