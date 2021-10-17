70df8b22c5633eb6b74c03f1fa77ecfd79111620
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-10-17 10:56:26 +0200

    Customer class done

--- U0K109_HFT_2021221.Models/Customer.cs
+++ U0K109_HFT_2021221.Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace U0K109_HFT_2021221.Models
 {
     public class Customer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [NotMapped]
+        public virtual ICollection<AppleProduct> Products { get; set; }
+        public Customer()
+        {
+            Products = new HashSet<AppleProduct>();
+        }
     }
 }
