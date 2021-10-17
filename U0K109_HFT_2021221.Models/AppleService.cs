afd6929aeb545c45adebb01db86fbbca4fe9adac
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-10-17 10:51:48 +0200

    AppleService class done

--- U0K109_HFT_2021221.Models/AppleService.cs
+++ U0K109_HFT_2021221.Models/AppleService.cs
@@ -7,6 +7,18 @@ namespace U0K109_HFT_2021221.Models
 {
     public class AppleService
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public string Location { get; set; }
+        [NotMapped]
+        public virtual ICollection<Customer> Customers { get; set; }
+        public virtual ICollection<AppleProduct> Products { get; set; }
+        public AppleService()
+        {
+            Customers = new HashSet<Customer>();
+            Products = new HashSet<AppleProduct>();
+        }
     }
 }
