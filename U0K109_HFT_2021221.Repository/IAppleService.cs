ccfae6c6985f5ceeac4d96be0d507331d0c11f8e
drop.dead <anonymous9832@gmail.com>
2021-11-14 12:35:21 +0100

    IAppleService interface filled

--- U0K109_HFT_2021221.Repository/IAppleService.cs
+++ U0K109_HFT_2021221.Repository/IAppleService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using U0K109_HFT_2021221.Models;
 
 namespace U0K109_HFT_2021221.Repository
 {
     public interface IAppleService
     {
+        void Create(AppleService appleService);
+        void Delete(int id);
+        IQueryable<AppleService> GetAll();
+        AppleService Read(int id);
+        void Update(AppleService appleService);
     }
 }
