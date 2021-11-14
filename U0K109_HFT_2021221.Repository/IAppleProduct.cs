af160ebbba1d88e297cb24c709c3ebe074124851
drop.dead <anonymous9832@gmail.com>
2021-11-14 12:37:42 +0100

    IAppleProduct interface filled

--- U0K109_HFT_2021221.Repository/IAppleProduct.cs
+++ U0K109_HFT_2021221.Repository/IAppleProduct.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Linq;
+using U0K109_HFT_2021221.Models;
 
 namespace U0K109_HFT_2021221.Repository
 {
     public interface IAppleProduct
     {
+        void Create(AppleProduct appleProduct);
+        void Delete(int id);
+        IQueryable<AppleProduct> GetAll();
+        AppleProduct Read(int id);
+        void Update(AppleProduct appleProduct);
     }
 }
