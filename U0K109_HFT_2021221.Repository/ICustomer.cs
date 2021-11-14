a835fd588fa18e290c0afbd9b0227584c09d0863
drop.dead <anonymous9832@gmail.com>
2021-11-14 12:33:37 +0100

    ICustomer interface filled

--- U0K109_HFT_2021221.Repository/ICustomer.cs
+++ U0K109_HFT_2021221.Repository/ICustomer.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using U0K109_HFT_2021221.Models;
 
 namespace U0K109_HFT_2021221.Repository
 {
     public interface ICustomer
     {
+        void Create(Customer customer);
+        void Delete(int id);   
+        IQueryable<Customer> GetAll();
+        Customer Read(int id);
+        void Update(Customer customer);
     }
 }
