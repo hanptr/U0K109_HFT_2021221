e453e8042113bd65a112a895a2e841dd433e196c
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-17 21:34:59 +0100

    AppleServiceLogic added

---
+++ U0K109_HFT_2021221.Logic/AppleServiceLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using U0K109_HFT_2021221.Models;
+using U0K109_HFT_2021221.Repository;
+
+namespace U0K109_HFT_2021221.Logic
+{
+    class AppleServiceLogic : IAppleServiceRepository
+    {
+        public void Create(AppleService appleService)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<AppleService> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppleService Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AppleService appleService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
