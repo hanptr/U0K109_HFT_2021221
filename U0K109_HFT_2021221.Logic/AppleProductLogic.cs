144d675fedbd502a068426e1e4cd5adca051afee
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-17 23:01:22 +0100

    AppleProductLogic interface implemented

--- U0K109_HFT_2021221.Logic/AppleProductLogic.cs
+++ U0K109_HFT_2021221.Logic/AppleProductLogic.cs
@@ -8,7 +8,36 @@ using U0K109_HFT_2021221.Repository;
 
 namespace U0K109_HFT_2021221.Logic
 {
-    class AppleProductLogic
+    class AppleProductLogic : IAppleProductLogic
     {
+        IAppleProductLogic appleProdcutRepo;
+        public AppleProductLogic(IAppleProductLogic appleProdcutRepo)
+        {
+            this.appleProdcutRepo = appleProdcutRepo;
+        }
+        public void Create(AppleProduct appleProduct)
+        {
+            appleProdcutRepo.Create(appleProduct);
+        }
+
+        public void Delete(int id)
+        {
+            appleProdcutRepo.Delete(id);
+        }
+
+        public IEnumerable<AppleProduct> GetAll()
+        {
+            return appleProdcutRepo.GetAll();
+        }
+
+        public AppleProduct Read(int id)
+        {
+            return appleProdcutRepo.Read(id);
+        }
+
+        public void Update(AppleProduct appleProduct)
+        {
+            appleProdcutRepo.Update(appleProduct);
+        }
     }
 }
