34a4e9138a7ee2e11af4ebdbd563a296bc83148b
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-17 21:32:47 +0100

    AppleProductLogic added

---
+++ U0K109_HFT_2021221.Logic/AppleProductLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using U0K109_HFT_2021221.Models;
+using U0K109_HFT_2021221.Repository;
+
+namespace U0K109_HFT_2021221.Logic
+{
+    class AppleProductLogic : IAppleProductRepository
+    {
+        public void Create(AppleProduct appleProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<AppleProduct> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppleProduct Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AppleProduct appleProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
