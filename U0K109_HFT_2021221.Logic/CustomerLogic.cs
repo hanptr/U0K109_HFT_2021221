6f361f412ea3e7e07330f2d7b3f78bf5993fef06
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-17 23:11:28 +0100

    CustomerLogic interface implemented

--- U0K109_HFT_2021221.Logic/CustomerLogic.cs
+++ U0K109_HFT_2021221.Logic/CustomerLogic.cs
@@ -8,8 +8,36 @@ using U0K109_HFT_2021221.Repository;
 
 namespace U0K109_HFT_2021221.Logic
 {
-    class CustomerLogic
+    class CustomerLogic : ICustomerLogic
     {
-        
+        ICustomerRepository customerRepo;
+        public CustomerLogic(ICustomerRepository customerRepo)
+        {
+            this.customerRepo = customerRepo;
+        }
+        public void Create(Customer customer)
+        {
+            customerRepo.Create(customer);
+        }
+
+        public void Delete(int id)
+        {
+            customerRepo.Delete(id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return customerRepo.GetAll();
+        }
+
+        public Customer Read(int id)
+        {
+            return customerRepo.Read(id);
+        }
+
+        public void Update(Customer customer)
+        {
+            customerRepo.Update(customer);
+        }
     }
 }
