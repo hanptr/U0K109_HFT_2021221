d7b7a9084f89bb0b6cfa448d937e666200470006
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-22 15:43:30 +0100

    nugets added to Test

--- U0K109_HFT_2021221.Test/Test.cs
+++ U0K109_HFT_2021221.Test/Test.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using Moq;
-//using System.Linq;
-//using NUnit.Framework;
-//using System.Collections.Generic;
-//using U0K109_HFT_2021221.Models;
-//using U0K109_HFT_2021221.Repository; 
-//using U0K109_HFT_2021221.Logic;
+﻿using System;
+using Moq;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using U0K109_HFT_2021221.Models;
+using U0K109_HFT_2021221.Repository;
+using U0K109_HFT_2021221.Logic;
 
-//namespace U0K109_HFT_2021221.Test
-//{
-//    [TestFixture]
-//    public class Test
-//    { 
-//        [SetUp]        
-//        public void MoqSetup()
-//        {
-            
-//        }
+namespace U0K109_HFT_2021221.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [SetUp]
+        public void MoqSetup()
+        {
 
-//        [Test]
-//        public void Test1()
-//        {
+        }
 
-//        }
+        [Test]
+        public void Test1()
+        {
 
-//    }
-//}
+        }
+
+    }
+}
