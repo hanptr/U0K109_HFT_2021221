6161444d8e5f64a19e4ef86a53269f3ac7828707
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2021-11-29 01:42:10 +0100

    Major changes in Program's Menu

--- U0K109_HFT_2021221.Client/Program.cs
+++ U0K109_HFT_2021221.Client/Program.cs
@@ -7,67 +7,36 @@ namespace U0K109_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Load..");
-            System.Threading.Thread.Sleep(8000);
-            RestService rest = new RestService("http://localhost:21980");
-            
-            
-            MainMenu();
+            Setup();
         }
+        static void Setup()
+        {
+            RestService rest = new RestService("http://localhost:21980");
+            MenuClass menu = new MenuClass();
 
-        static void MainMenu()
+            menu.clear += Clear;
+            menu.input += Input;
+            menu.write += Write;
+            menu.writeline += WriteLine;
+
+            menu.Start(rest);
+        }
+        static void Clear()
         {
             Console.Clear();
-            string choice = "";
-            while (choice!="Quit")
-            {
-                Console.WriteLine("Apple menu");
-                Console.WriteLine("\n1. AppleService");
-                Console.WriteLine("2. Customer");
-                Console.WriteLine("3. AppleProduct");
-                Console.WriteLine("4. Quit");
-                Console.Write("\nYour choice: ");
-                choice = Console.ReadLine();
-                switch (choice)
-                {
-                    case "1":
-
-                    default:
-                        break;
-                }
-            }
         }
-        //AppleService
-        static void AppleServiceOption()
+        static string Input()
         {
-            RestService rest = new RestService("http://localhost:21980");
-            Console.WriteLine("Apple menu/AppleService");
-            Console.WriteLine("\n1.Get all data from the database regarding the Apple services. ");
-            Console.WriteLine("2. Customer");
-            Console.WriteLine("3. AppleProduct");
-            Console.WriteLine("4. Quit");
-            Console.Write("\nYour choice: ");
-            string choice2 = Console.ReadLine();
-            switch (choice2)
-            {
-                case "1":
-                    var result=rest.Get<AppleService>("AppleService");
-                    Console.WriteLine("Apple services and details:");
-                    foreach (var item in result)
-                    {
-                        Console.WriteLine("Apple service's name: "+item.ServiceName+"\n");
-                        Console.WriteLine("Apple service's ID: " + item.ServiceID + "\n");
-                        Console.WriteLine("Apple service's location: " + item.Location + "\n");
-                        Console.WriteLine("Apple service's products: " + item.Products + "\n");
-                        Console.WriteLine("Apple service's customers: " + item.Customers + "\n");
-
-                    }
-                    break;
-                case
-
-                default:
-                    break;
-            }
+            return Console.ReadLine();
         }
+        static void Write(string s)
+        {
+            Console.Write(s);
+        }
+        static void WriteLine(string s)
+        {
+            Console.WriteLine(s);
+        }
+
     }
 }
