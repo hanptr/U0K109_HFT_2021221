5a08214811f052cea17643eab04d34cfa3966c52
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2022-03-27 18:56:23 +0200

    MainWindowViewModel created

---
+++ U0K109_HFT_2021221.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using U0K109_HFT_2021221.Models;
+
+namespace U0K109_HFT_2021221.WpfClient
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<AppleService> AppleServices { get; set; }
+        private AppleService selectedAppleService;
+
+        public AppleService SelectedAppleService
+        {
+            get { return selectedAppleService; }
+            set
+            { 
+                SetProperty(ref selectedAppleService, value);
+                (DeleteAppleServiceCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        public ICommand CreateAppleServiceCommand { get; set; }
+        public ICommand DeleteAppleServiceCommand { get; set; }
+        public ICommand UpdateAppleServiceCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            AppleServices = new RestCollection<AppleService>("http://localhost:21980/", "appleService");
+            CreateAppleServiceCommand = new RelayCommand(
+                () => 
+                {
+                    AppleServices.Add(new AppleService()
+                    {
+                        ServiceName="a5",
+                        Location="Sajt"
+                        
+                    }
+                        );
+                }
+                );
+            DeleteAppleServiceCommand = new RelayCommand(
+                () =>
+                {
+                    AppleServices.Delete(SelectedAppleService.ServiceID);
+                },
+                ()=>
+                {
+                    return (SelectedAppleService != null && SelectedAppleService.Products.Count()==0);
+                });
+
+        }
+    }
+}
